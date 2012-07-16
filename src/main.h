611e862fc19c7a5ce448d28059a0ef1621d43ec1
Kalle Hyvönen <kalle.hyvonen@dgkelectronics.com>
2012-07-16 20:00:30 +0300

    Added #defines for the adapter to the code header

---
+++ src/main.h
@@ -0,0 +1,27 @@
+#define DIOI1 PORTB0
+#define DIOI2 PORTB1
+#define DIOI3 PORTB2
+#define DIOI4 PORTB3
+#define DIOI5 PORTB4
+#define DIOI6 PORTB5
+#define DIOI7 PORTB6
+#define DIOI8 PORTB7
+
+#define IFC PORTC6
+#define REN PORTC7
+
+#define DAV PORTD0
+#define ATN PORTD1
+#define EOI PORTD4
+#define SRQ PORTD5
+#define NRFD PORTD6
+#define NDAC PORTD7
+
+#define TE PORTE6
+
+#define GPIO1 PORTF0
+#define GPIO2 PORTF1
+#define GPIO3 PORTF4
+#define GPIO4 PORTF5
+#define GPIO5 PORTF6
+#define DC PORTF7
